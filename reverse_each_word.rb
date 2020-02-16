e28e43f5cfa6da64d6402552b186176f3db63f5e
Tyler Lai <lai.t.tyler@gmail.com>
2020-02-16 00:11:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,10 +8,4 @@ def reverse_each_word(word)
   arr = word.split()
   result = arr.collect {|w| w.reverse}
   result.join(" ")
-  
-  # new_string = ""
-  # array.collect do |word|
-  #   new_string << "#{word.reverse} "
-  # end
-  # new_string.rstrip
 end
